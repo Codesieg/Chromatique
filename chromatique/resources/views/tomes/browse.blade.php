1e3a4d550e774c621d7fc7472187f33bde42abf1
codesieg <sieg.heart@hotmail.fr>
2022-08-22 14:27:45 +0200

    ajout tous les mangas

--- chromatique/resources/views/tomes/browse.blade.php
+++ chromatique/resources/views/tomes/browse.blade.php
@@ -8,8 +8,9 @@
                 <div class="col-lg-12">
                     <div class="breadcrumb__links">
                         <a href="./index.html"><i class="fa fa-home"></i> Home</a>
-                        <a href="./categories.html">Categories</a>
-                        <span>Adventure</span>
+                        <!-- TODO : Création du breadcrumb
+                         <a href="./categories.html">Categories</a>
+                        <span>Adventure</span> -->
                     </div>
                 </div>
             </div>
@@ -24,17 +25,17 @@
                 <div class="row">
                     <div class="col-lg-3">
                         <div class="anime__details__pic set-bg" data-setbg="<?= asset('storage/mangas/') ?>{{$mangaDetails->manga_directory . "/" . $mangaDetails->manga_cover }}">
-                            <div class="comment"><i class="fa fa-comments"></i> 11</div>
-                            <div class="view"><i class="fa fa-eye"></i> 9141</div>
+                            <!--TODO <div class="comment"><i class="fa fa-comments"></i> 11</div>
+                            <div class="view"><i class="fa fa-eye"></i> 9141</div> -->
                         </div>
                     </div>
                     <div class="col-lg-9">
                         <div class="anime__details__text">
                             <div class="anime__details__title">
                                 <h3>{{ $mangaDetails->manga_name }}</h3>
-                                {{-- <span>フェイト／ステイナイト, Feito／sutei naito</span> --}}
+                                <!-- <span>フェイト／ステイナイト, Feito／sutei naito</span> -->
                             </div>
-                            <div class="anime__details__rating">
+                            <!-- TODO <div class="anime__details__rating">
                                 <div class="rating">
                                     <a href="#"><i class="fa fa-star"></i></a>
                                     <a href="#"><i class="fa fa-star"></i></a>
@@ -43,34 +44,35 @@
                                     <a href="#"><i class="fa fa-star-half-o"></i></a>
                                 </div>
                                 <span>1.029 Votes</span>
-                            </div>
+                            </div> -->
                             <p>{{ $mangaDetails->manga_synopsis }}</p>
                             <div class="anime__details__widget">
                                 <div class="row">
                                     <div class="col-lg-6 col-md-6">
                                         <ul>
-                                            <li><span>Type :</span>Shomen</li>
+                                            <!-- TODO : <li><span>Type :</span>Shomen</li> -->
                                             <li><span>Autheur :</span>{{ $mangaDetails->manga_author }}</li>
                                             <li><span>Date d'ajout :</span> {{ $mangaDetails->updated_at }}</li>
                                             <li><span>Status :</span> En cours</li>
-                                            <li><span>Genre :</span> Action, Adventure, Fantasy, Magic</li>
+                                            <li><span>Uploadeur :</span> {{ $uploader->name }}</li>
+                                            <li><span>Colorisé par :</span> Sardan</li>
+                                            <!-- TODO <li><span>Genre :</span> Action, Adventure, Fantasy, Magic</li> -->
                                         </ul>
                                     </div>
                                     <div class="col-lg-6 col-md-6">
                                         <ul>
-                                            <li><span>Scores :</span> 7.31 / 1,515</li>
+                                            <!-- TODO <li><span>Scores :</span> 7.31 / 1,515</li>
                                             <li><span>Votes :</span> 8.5 / 161 times</li>
-                                            <li><span>Vues :</span> 131,541</li>
-                                            <li><span>Uploadeur :</span> {{ $uploader->name }}</li>
-                                            <li><span>Colorisé par :</span> Sardan</li>
+                                            <li><span>Vues :</span> 131,541</li> -->
+                                            
                                         </ul>
                                     </div>
                                 </div>
                             </div>
                             <div class="anime__details__btn">
-                                <a href="#" class="follow-btn"><i class="fa fa-heart-o"></i> Suivre</a>
-                                {{-- <a href="#" class="watch-btn"><span>Watch Now</span> <i
-                                    class="fa fa-angle-right"></i></a> --}}
+                                <!-- TODO <a href="#" class="follow-btn"><i class="fa fa-heart-o"></i> Suivre</a>
+                                 <a href="#" class="watch-btn"><span>Watch Now</span> <i
+                                    class="fa fa-angle-right"></i></a> -->
                                 </div>
                             </div>
                         </div>
@@ -91,7 +93,7 @@
                             @endforeach 
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-4">
+                    <!-- TODO <div class="col-lg-4 col-md-4">
                         <div class="anime__details__sidebar">
                             <div class="section-title">
                                 <h5>Vous aimeriez...</h5>
@@ -117,7 +119,7 @@
                                 <h5><a href="#">Fate/stay night: Heaven's Feel I. presage flower</a></h5>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </section>
